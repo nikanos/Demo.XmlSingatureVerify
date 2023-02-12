3d44ac49aec16cb7edd19edfb7ec430fff75be2e
Nikanos Polykarpou <nikanos@windowslive.com>
2023-02-12 21:57:34 +0200

    addition of CLI (initial skeleton)

---
+++ Demo.XmlSignatureVerify.CLI/Options.cs
@@ -0,0 +1,14 @@
+﻿using CommandLine;
+
+namespace Demo.XmlSignatureVerify.CLI
+{
+    class Options
+    {
+        [Option('i', "input", Required = true, HelpText = "Input signed XML file.")]
+        public string InputFile { get; set; }
+
+        [Option('n', "name", Required = false, HelpText = "Reference ID attribute name")]
+        public string ReferenceIdAttributeName { get; set; }
+
+    }
+}
---
+++ Demo.XmlSignatureVerify.CLI/Program.cs
@@ -0,0 +1,34 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.XmlSignatureVerify.CLI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions)
+                .WithNotParsed(HandleParseError);
+        }
+
+        static void RunOptions(Options opts)
+        {
+            try
+            {
+                //TODO. Implement CLI
+            }
+            catch (Exception e)
+            {
+                Console.Error.WriteLine(e.ToString());
+            }
+        }
+        static void HandleParseError(IEnumerable<Error> errs)
+        {
+        }
+    }
+}
